d4ff0d876c7faa289dd5808bc5a3aed4e5f20e29
rflishrl <rafliworkspace@gmail.com>
2023-02-15 09:47:35 +0700

    add: upload files

--- api.js
+++ api.js
@@ -2,11 +2,29 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const mysql = require("mysql")
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static(__dirname));
+app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // set file storage
+        cb(null, './image');
+    },
+    filename: (req, file, cb) => {
+        // generate file name 
+        cb(null, "image-" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+let upload = multer({ storage: storage })
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -191,61 +209,86 @@ app.get("/kendaraan/:id", (req, res) => {
     })
 })
 
-app.post("/kendaraan", (req, res) => {
-
+app.post("/kendaraan", upload.single("image"), (req, res) => {
     let data = {
         nopol: req.body.nopol,
         warna: req.body.warna,
-        kondisi_kendaraan: req.body.kondisi_kendaraan
+        kondisi_kendaraan: req.body.kondisi_kendaraan,
+        image: req.file.filename
     }
 
-    let sql = "insert into kendaraan set ?"
+    if (!req.file) {
+        // jika tidak ada file yang diupload
+        res.json({
+            message: "No files sent"
+        })
+    } else {
+        // create sql insert
+        let sql = "insert into kendaraan set ?"
 
-    db.query(sql, data, (error, result) => {
-        let response = null
-        if (error) {
-            response = {
-                message: error.message
-            }
-        } else {
-            response = {
+        // run query
+        db.query(sql, data, (error, result) => {
+            if (error) throw error
+            res.json({
                 message: result.affectedRows + " data inserted"
-            }
-        }
-        res.json(response)
-    })
+            })
+        })
+    }
 })
 
-app.put("/kendaraan", (req, res) => {
 
-    let data = [
-        {
+app.put("/kendaraan", upload.single("image"), (req, res) => {
+    let data = null, sql = null
+
+    let param = { id_kendaraan: req.body.id_kendaraan }
+
+    if (!req.file) {
+
+        data = {
             nopol: req.body.nopol,
             warna: req.body.warna,
             kondisi_kendaraan: req.body.kondisi_kendaraan
-        },
-
-        {
-            id_kendaraan: req.body.id_kendaraan
         }
-    ]
+    } else {
 
-    let sql = "update kendaraan set ? where ?"
+        data = {
+            nopol: req.body.nopol,
+            warna: req.body.warna,
+            kondisi_kendaraan: req.body.kondisi_kendaraan,
+            image: req.file.filename
+        }
 
-    db.query(sql, data, (error, result) => {
-        let response = null
+        sql = "select * from kendaraan where ?"
+        // run query
+        db.query(sql, param, (err, result) => {
+            if (err) throw err
+            // tampung nama file yang lama
+            let fileName = result[0].image
+
+            // hapus file yg lama
+            let dir = path.join(__dirname, "image", fileName)
+            fs.unlink(dir, (error) => { })
+        })
+
+    }
+
+    // create sql update
+    sql = "update kendaraan set ? where ?"
+
+    // run sql update
+    db.query(sql, [data, param], (error, result) => {
         if (error) {
-            response = {
+            res.json({
                 message: error.message
-            }
+            })
         } else {
-            response = {
+            res.json({
                 message: result.affectedRows + " data updated"
-            }
+            })
         }
-        res.json(response)
     })
 })
+
 
 app.delete("/kendaraan/:id", (req, res) => {
     let data = {
@@ -315,7 +358,6 @@ app.get("/admin/:id", (req, res) => {
 })
 
 app.post("/admin", (req, res) => {
-
     let data = {
         nama_admin: req.body.nama_admin,
         status_admin: req.body.status_admin
@@ -339,7 +381,6 @@ app.post("/admin", (req, res) => {
 })
 
 app.put("/admin", (req, res) => {
-
     let data = [
         {
             nama_admin: req.body.nama_admin,
@@ -390,6 +431,6 @@ app.delete("/admin/:id", (req, res) => {
     })
 })
 
-app.listen(8000, () => {
-    console.log("Run on port 8000")
+app.listen(3000, () => {
+    console.log("Run on port 3000")
 })
